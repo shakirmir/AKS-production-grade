d24a4f6a7fdff5dcdef4abcd80c73a038e79ea8a
shakirmir <shakirmir@gmail.com>
2026-07-25 22:25:00 +0530

    Fix network policy to use calico instead of none

--- infra/modules/aks/main.tf
+++ infra/modules/aks/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   network_profile {
     network_plugin    = "azure"
-    network_policy    = "none"
+    network_policy    = "calico"
     load_balancer_sku = "standard"
   }
 
